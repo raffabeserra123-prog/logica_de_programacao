c3b6f28ad16a85513337db62a2cd17a7c86c342e
raffabeserra123-prog <raffa.beserra123@gmail.com>
2026-06-23 11:03:22 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,120 @@
+/*
+Let valor1= 10
+let valor2= 7
+let resultado= valor1 + valor2
+console.log(resultado)
+*/
+
+/*
+let valor1= 36
+let valor2= 16
+let resultado= valor1-valor2
+console.log(valor1-valor2)
+*/
+
+/*
+let valor1= 180
+let valor2= 60
+let resultado= valor1*valor2
+console.log(valor1*valor2)
+*/
+
+/*
+let valor1= 18
+let valor2= 6
+let resultado= valor1/valor2
+console.log(valor1/valor2)
+*/
+
+/*
+let valor1= 10
+let valor2= 10
+let resultado= valor1&gt;valor2
+console.log(valor1&gt;valor2)
+*/
+
+/*
+let valor1= 20
+let valor2= 10
+let resultado= valor1&lt;valor2
+console.log(valor1&lt;valor2)
+*/
+
+/*
+let valor1= 30
+let valor2= 10
+let resultado= valor1&lt;=valor2
+console.log(valor1&lt;=valor2)
+*/
+
+/*
+let valor1= 25
+let valor2= 15
+let resultado= valor1&gt;=valor2
+console.log(valor1&gt;=valor2 )
+*/
+
+/*
+let valor1= 70
+let valor2= 65
+let resultado= valor1==valor2
+console.log(valor1==valor2)
+*/
+
+/*
+let valor1= 26
+
+let valor2= 17
+let resultado= valor1===valor2
+console.log(valor1===valor2)
+*/
+
+/*
+let valor1= 20
+let valor2= &quot;9&quot;
+let resultado= valor1+valor2
+console.log(valor1+valor2)
+*/
+
+/*
+let valor1= 21
+valor1 ++
+let valor2= 9
+let resultado= valor1+valor2
+console.log(valor1+valor2)
+*/
+
+/*
+let valor1= 17
+valor1 --
+let valor2= 8
+let resultado= valor1-valor2
+console.log(valor1-valor2)
+*/
+
+/*
+let valor1= 19
+let valor2= 11
+let resultado= valor1+=valor2
+*/
+
+/*
+let valor1 = 548
+let valor2 = 67
+valor1 += valor2
+console.log(valor1)
+*/
+
+/*
+let valor1= 88
+let valor2= 78
+let resultado= valor1!=valor2
+console.log(valor1!=valor2)
+*/
+
+/*
+let valor1= 26
+let valor2= 29
+let resultado= valor1!==valor2
+console.log(valor1!==valor2)
+*/
